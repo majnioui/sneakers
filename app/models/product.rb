7a09af24c2632a8caab808ef378bf3cc580d062b
Mohammed <majnioui@gmail.com>
2022-09-18 00:59:53 +0100

    add categories to products and navigate to them from the same path

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ApplicationRecord
     has_many :order_items, dependent: :destroy
     has_rich_text :description
     has_one :action_text_rich_text, class_name: 'ActionText::RichText', as: :record
+    validates :category_id, presence: true
+    belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   get 'users/show'
   get 'users/edit'
   get 'users/update'
